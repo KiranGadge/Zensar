ab22c586ce442c0ceadb39c1f285b3a584a3297c
kiran <kiran@DESKTOP-5E8P7V8>
2021-09-01 19:47:58 +0530

    CompareTo method

---
+++ Zensar/src/CompareTo/CustomSort.java
@@ -0,0 +1,27 @@
+package CompareTo;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class CustomSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<Student1> al=new ArrayList<>();
+		al.add(new Student1("A", "BE",9766646343l,90));
+		al.add(new Student1("FF", "BE",9175900787l,89));
+		al.add(new Student1("E", "BE",9766646343l,91));
+		al.add(new Student1("CCCC", "BE",9175900767l,92));
+		al.add(new Student1("BBB", "BE",9766646343l,93));
+		al.add(new Student1("D", "BE",9175900767l,90));
+		
+		
+		Collections.sort(al);
+		
+		for(Student1 ob:al)
+			System.out.println(ob);
+
+
+	}
+
+}
---
+++ Zensar/src/CompareTo/Student1.java
@@ -0,0 +1,75 @@
+package CompareTo;
+
+public class Student1 implements Comparable<Student1>
+{
+	private String name;
+	private String qualification;
+	private long mobile;
+	private int percent;
+	
+public Student1() {
+		
+	}
+
+	public Student1(String name, String qualification, long mobile, int percent) {
+		super();
+		this.name = name;
+		this.qualification = qualification;
+		this.mobile = mobile;
+		this.percent = percent;
+	}
+	
+	
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getQualification() {
+		return qualification;
+	}
+
+	public void setQualification(String qualification) {
+		this.qualification = qualification;
+	}
+
+	public long getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(long mobile) {
+		this.mobile = mobile;
+	}
+
+	public int getPercent() {
+		return percent;
+	}
+
+	public void setPercent(int percent) {
+		this.percent = percent;
+	}
+
+	@Override
+	public String toString() {
+		return "Student has--> name=" + name + ", qualification=" + qualification + ", mobile=" + mobile + ", percent="
+				+ percent ;
+	}
+
+	
+	public int compareTo(Student1 o) {
+	
+		if(o.name.length()<this.name.length())
+			return -1;
+		else if(o.name.length()>this.name.length())
+			return +1;
+		else 
+			return 0;
+			
+		
+		
+	}
+}
